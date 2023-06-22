21749f445c1417a1866b6e36f0fcc60675098e91
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-06-22 13:44:24 +0300

    addmin custumaization

--- Tender_app/E_Tender/admin.py
+++ Tender_app/E_Tender/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from.import models
 
 # Register your models here.
+admin.site.site_header='E-Tender Administrator'
+admin.site.site_title='Tender Aoutomation System'
+admin.site.index_title='Well come to E-Tender'
