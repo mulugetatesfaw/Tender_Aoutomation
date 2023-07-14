982f8a78065d78330325f2e7b9c0773d003b2be6
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-07-14 14:15:13 +0300

    final clean up for the whole backend

--- Tender_app/Tender_app/settings.py
+++ Tender_app/Tender_app/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--noop=0v-p44$6n^1)w1g7mmk$=x^-$+=5ui6=w)$!vo)6xv#t'
+# Generate a new secret key
+from django.core.management.utils import get_random_secret_key
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
